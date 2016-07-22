1535560776a9e07d77426c0ae65c36c937aac8b6
Sidnei Weber <sidnei.weber@gmail.com>
2016-07-22 12:50:10 -0300

    [UPDATE] remover arquivos com mais de 7 dias

--- backup-servidor.sh
+++ backup-servidor.sh
@@ -8,3 +8,6 @@ mysqldump -u root -psenha catalogo > /tmp/catalogo-"$DIA"-"$DATA".sql
 
 # Compactar pasta, junto com backup mysql
 tar -zcvf /home/sidnei/MEGAsync/Branco/catalogo-"$DIA"-"$DATA".tar.gz /var/www/catalogo/ /tmp/catalogo-"$DIA"-"$DATA".sql
+
+# Apagar arquivos com mais de sete dias
+find /home/sidnei/MEGAsync/Branco/ ! -mtime -7 | /usr/bin/xargs rm -rf
